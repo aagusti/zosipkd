ed1f54716f33271f9aed3bcc1120d2477e730e13
aagusti <aa.gustiana@gmail.com>
2015-01-03 14:04:36 +0700

    deleted:    docs/.~lock.KODE REKENING PAJAK PEMDA KOTA BOGOR.csv# 	deleted:    docs/config.py 	deleted:    docs/eis-row-level-dmyd.sql 	deleted:    docs/esi.sql 	deleted:    docs/gaji_pns.backup 	deleted:    docs/import-gaji.py 	deleted:    docs/import-rekening.sql 	deleted:    docs/import-units.py 	deleted:    docs/import-user.py 	deleted:    docs/rekening_bogor1.csv 	deleted:    docs/rekening_bogor1.ods 	deleted:    docs/route.txt 	deleted:    docs/routes.csv 	deleted:    docs/savedoc.txt 	deleted:    docs/test.sql 	deleted:    osipkd/views/anggaran/templates/gajipegawai/list.pt 	deleted:    osipkd/views/anggaran/templates/gajipotongan/add.pt 	deleted:    osipkd/views/anggaran/templates/gajipotongan/delete.pt 	deleted:    osipkd/views/anggaran/templates/gajipotongan/list.pt 	deleted:    osipkd/views/pendapatan/__init__.py 	deleted:    osipkd/views/pendapatan/ar_invoice.py 	deleted:    osipkd/views/pendapatan/ar_payment.py 	deleted:    osipkd/views/pendapatan/ar_report.py 	deleted:    osipkd/views/pendapatan/templates/ar-invoice-item/add.pt 	deleted:    osipkd/views/pendapatan/templates/ar-invoice-item/delete.pt 	deleted:    osipkd/views/pendapatan/templates/ar-invoice-item/list.pt 	deleted:    osipkd/views/pendapatan/templates/ar-payment-item/add.pt 	deleted:    osipkd/views/pendapatan/templates/ar-payment-item/delete.pt 	deleted:    osipkd/views/pendapatan/templates/ar-payment-item/list.pt 	deleted:    osipkd/views/pendapatan/templates/ar-report-item/add.pt 	deleted:    osipkd/views/pendapatan/templates/ar-report-item/delete.pt 	deleted:    osipkd/views/pendapatan/templates/ar-report-item/list.pt 	deleted:    osipkd/views/pendapatan/templates/base.pt 	deleted:    osipkd/views/pendapatan/templates/home.pt

--- docs/import-rekening.sql
+++
@@ -1,7 +0,0 @@
-/*INSERT INTO admin.rekenings(
-            id, )
-    VALUES (?, ?, ?, ?, ?, ?, ?, ?, 
-            ?, ?, ?);
-*/
-COPY admin.rekenings (kode, nama, level_id, tahun, disabled, created, updated, 
-           create_uid) FROM '/tmp/rekening_bogor1.csv' DELIMITER ',' HEADER CSV;
