cf2af9f978c2b088451278ed18948d7f35c25bd8
aagusti <aa.gustiana@gmail.com>
2015-01-26 10:55:26 +0700

    update route all

--- docs/route_all.sql
+++ docs/route_all.sql
@@ -277,3 +277,91 @@ ap-spm-potongan-act	0	2014-12-24 03:04:42.837591	1	SPM Potongan Act	/ap-spm-poto
 ap-spm-potongan-add	0	2014-12-24 03:04:42.837591	1	Tambah SPM Potongan	/ap-spm-potongan/{ap_spm_id}/add	add
 ap-spm-potongan-edit	0	2014-12-24 03:04:42.837591	1	Edit SPM Potongan	/ap-spm-potongan/{ap_spm_id}/{id}/edit	edit
 ap-spm-potongan-delete	0	2014-12-24 03:04:42.837591	1	Hapus SPM Potongan	/ap-spm-potongan/{ap_spm_id}/{id}/delete	delete
+p-ap-invoice-skpd	0	2014-12-24 03:04:42.837591	1	Posting Tagihan/Utang	/p-ap-invoice-skpd	view
+p-ap-invoice-skpd-act	0	2014-12-24 03:04:42.837591	1	Posting Tagihan/Utang Act	/p-ap-invoice-skpd/act/{act}	read
+p-ap-sp2d-skpd	0	2014-12-24 03:04:42.837591	1	Posting SP2D	/p-ap-sp2d-skpd	view
+p-ap-sp2d-skpd-act	0	2014-12-24 03:04:42.837591	1	Posting SP2D Act	/p-ap-sp2d-skpd/act/{act}	read
+aset	0	2014-12-24 03:04:42.837591	1	Aset	/aset	read
+aset-kategori	0	2014-12-24 03:04:42.837591	1	Kategori Aset	/aset/kategori	read
+aset-kategori-add	0	2014-12-24 03:04:42.837591	1	Tambah Kategori Aset	/aset/kategori/add	read
+aset-kategori-edit	0	2014-12-24 03:04:42.837591	1	Edit Kategori Aset	/aset/kategori/{id}/edit	edit
+aset-kategori-delete	0	2014-12-24 03:04:42.837591	1	Hapus Kategori Aset	/aset/kategori/{id}/delete	delete
+aset-kategori-act	0	2014-12-24 03:04:42.837591	1	Kategori Aset Act	/aset/kategori/{act}/act	read
+aset-kategori-csv	0	2014-12-24 03:04:42.837591	1	Kategori Aset CSV	/aset/kategori/{csv}/csv	read
+aset-kebijakan	0	2014-12-24 03:04:42.837591	1	Kebijakan Aset	/aset/kebijakan	read
+aset-kebijakan-add	0	2014-12-24 03:04:42.837591	1	Tambah Kebijakan Aset	/aset/kebijakan/add	read
+aset-kebijakan-edit	0	2014-12-24 03:04:42.837591	1	Edit Kebijakan Aset	/aset/kebijakan/{id}/edit	edit
+aset-kebijakan-delete	0	2014-12-24 03:04:42.837591	1	Hapus Kebijakan Aset	/aset/kebijakan/{id}/delete	delete
+aset-kebijakan-act	0	2014-12-24 03:04:42.837591	1	Kebijakan Aset Act	/aset/kebijakan/{act}/act	read
+aset-kebijakan-csv	0	2014-12-24 03:04:42.837591	1	Kebijakan Aset CSV	/aset/kebijakan/{csv}/csv	read
+aset-mutasi	0	2014-12-24 03:04:42.837591	1	Mutasi Aset	/aset/mutasi	read
+aset-mutasi-add	0	2014-12-24 03:04:42.837591	1	Tambah Mutasi Aset	/aset/mutasi/add	read
+aset-mutasi-edit	0	2014-12-24 03:04:42.837591	1	Edit Mutasi Aset	/aset/mutasi/{id}/edit	edit
+aset-mutasi-delete	0	2014-12-24 03:04:42.837591	1	Hapus Mutasi Aset	/aset/mutasi/{id}/delete	delete
+aset-mutasi-act	0	2014-12-24 03:04:42.837591	1	Mutasi Aset Act	/aset/mutasi/{act}/act	read
+aset-mutasi-csv	0	2014-12-24 03:04:42.837591	1	Mutasi Aset CSV	/aset/mutasi/{csv}/csv	read
+aset-kiba	0	2014-12-24 03:04:42.837591	1	KIB A (Tanah)	/aset/kiba	read
+aset-kiba-add	0	2014-12-24 03:04:42.837591	1	Tambah KIB A (Tanah)	/aset/kiba/add	add
+aset-kiba-edit	0	2014-12-24 03:04:42.837591	1	Edit KIB A (Tanah)	/aset/kiba/{id}/edit	edit
+aset-kiba-delete	0	2014-12-24 03:04:42.837591	1	Hapus KIB A (Tanah)	/aset/kiba/{id}/delete	delete
+aset-kiba-act	0	2014-12-24 03:04:42.837591	1	KIB A (Tanah) Act	/aset/kiba/{act}/act	read
+aset-kiba-csv	0	2014-12-24 03:04:42.837591	1	KIB A (Tanah) CSV	/aset/kiba/{csv}/csv	read
+aset-kibb	0	2014-12-24 03:04:42.837591	1	KIB KIB B (Peralatan dan Mesin)	/aset/kibb	read
+aset-kibb-add	0	2014-12-24 03:04:42.837591	1	Tambah KIB KIB B (Peralatan dan Mesin)	/aset/kibb/add	add
+aset-kibb-edit	0	2014-12-24 03:04:42.837591	1	Edit KIB KIB B (Peralatan dan Mesin)	/aset/kibb/{id}/edit	edit
+aset-kibb-delete	0	2014-12-24 03:04:42.837591	1	Hapus KIB KIB B (Peralatan dan Mesin)	/aset/kibb/{id}/delete	delete
+aset-kibb-act	0	2014-12-24 03:04:42.837591	1	KIB KIB B (Peralatan dan Mesin) Act	/aset/kibb/{act}/act	read
+aset-kibb-csv	0	2014-12-24 03:04:42.837591	1	KIB KIB B (Peralatan dan Mesin) CSV	/aset/kibb/{csv}/csv	read
+aset-kibc	0	2014-12-24 03:04:42.837591	1	KIB C (Bangunan)	/aset/kibc	read
+aset-kibc-add	0	2014-12-24 03:04:42.837591	1	Tambah KIB C (Bangunan)	/aset/kibc/add	add
+aset-kibc-edit	0	2014-12-24 03:04:42.837591	1	Edit KIB C (Bangunan)	/aset/kibc/{id}/edit	edit
+aset-kibc-delete	0	2014-12-24 03:04:42.837591	1	Hapus KIB C (Bangunan)	/aset/kibc/{id}/delete	delete
+aset-kibc-act	0	2014-12-24 03:04:42.837591	1	KIB C (Bangunan) Act	/aset/kibc/{act}/act	read
+aset-kibc-csv	0	2014-12-24 03:04:42.837591	1	KIB C (Bangunan) CSV	/aset/kibc/{csv}/csv	read
+aset-kibd	0	2014-12-24 03:04:42.837591	1	KIB D (Jalan, jaringan dan Irigasi)	/aset/kibd	read
+aset-kibd-add	0	2014-12-24 03:04:42.837591	1	Tambah KIB D (Jalan, jaringan dan Irigasi)	/aset/kibd/add	add
+aset-kibd-edit	0	2014-12-24 03:04:42.837591	1	Edit KIB D (Jalan, jaringan dan Irigasi)	/aset/kibd/{id}/edit	edit
+aset-kibd-delete	0	2014-12-24 03:04:42.837591	1	Hapus KIB D (Jalan, jaringan dan Irigasi)	/aset/kibd/{id}/delete	delete
+aset-kibd-act	0	2014-12-24 03:04:42.837591	1	KIB D (Jalan, jaringan dan Irigasi) Act	/aset/kibd/{act}/act	read
+aset-kibd-csv	0	2014-12-24 03:04:42.837591	1	KIB D (Jalan, jaringan dan Irigasi) CSV	/aset/kibd/{csv}/csv	read
+aset-kibe	0	2014-12-24 03:04:42.837591	1	KIB E (Aset Lainnya)	/aset/kibe	read
+aset-kibe-add	0	2014-12-24 03:04:42.837591	1	Tambah KIB E (Aset Lainnya)	/aset/kibe/add	add
+aset-kibe-edit	0	2014-12-24 03:04:42.837591	1	Edit KIB E (Aset Lainnya)	/aset/kibe/{id}/edit	edit
+aset-kibe-delete	0	2014-12-24 03:04:42.837591	1	Hapus KIB E (Aset Lainnya)	/aset/kibe/{id}/delete	delete
+aset-kibe-act	0	2014-12-24 03:04:42.837591	1	KIB E (Aset Lainnya) Act	/aset/kibe/{act}/act	read
+aset-kibe-csv	0	2014-12-24 03:04:42.837591	1	KIB E (Aset Lainnya) CSV	/aset/kibe/{csv}/csv	read
+aset-kibf	0	2014-12-24 03:04:42.837591	1	KIB F (Konstruksi dalam Pengerjaan)	/aset/kibf	read
+aset-kibf-add	0	2014-12-24 03:04:42.837591	1	Tambah KIB F (Konstruksi dalam Pengerjaan)	/aset/kibf/add	add
+aset-kibf-edit	0	2014-12-24 03:04:42.837591	1	Edit KIB F (Konstruksi dalam Pengerjaan)	/aset/kibf/{id}/edit	edit
+aset-kibf-delete	0	2014-12-24 03:04:42.837591	1	Hapus KIB F (Konstruksi dalam Pengerjaan)	/aset/kibf/{id}/delete	delete
+aset-kibf-act	0	2014-12-24 03:04:42.837591	1	KIB F (Konstruksi dalam Pengerjaan) Act	/aset/kibf/{act}/act	read
+aset-kibf-csv	0	2014-12-24 03:04:42.837591	1	KIB F (Konstruksi dalam Pengerjaan) CSV	/aset/kibf/{csv}/csv	read
+aset-pemilik	0	2014-12-24 03:04:42.837591	1	Pemilik	/aset/pemilik	read
+aset-pemilik-add	0	2014-12-24 03:04:42.837591	1	Tambah Pemilik	/aset/pemilik/add	add
+aset-pemilik-edit	0	2014-12-24 03:04:42.837591	1	Edit Pemilik	/aset/pemilik/{id}/edit	edit
+aset-pemilik-delete	0	2014-12-24 03:04:42.837591	1	Hapus Pemilik	/aset/pemilik/{id}/delete	delete
+aset-pemilik-act	0	2014-12-24 03:04:42.837591	1	Pemilik Act	/aset/pemilik/{act}/act	read
+aset-pemilik-csv	0	2014-12-24 03:04:42.837591	1	Pemilik CSV	/aset/pemilik/{csv}/csv	read
+efiling	0	2014-12-24 03:04:42.837591	1	Filing	/efiling	read
+efiling-admin	0	2014-12-24 03:04:42.837591	1	Admin e-Filling	/efiling/admin	read
+efiling-kategori	0	2014-12-24 03:04:42.837591	1	Kategori Filing	/efiling/kategori	read
+efiling-kategori-add	0	2014-12-24 03:04:42.837591	1	Tambah Kategori Filing	/efiling/kategori/add	read
+efiling-kategori-edit	0	2014-12-24 03:04:42.837591	1	Edit Kategori Filing	/efiling/kategori/{id}/edit	edit
+efiling-kategori-delete	0	2014-12-24 03:04:42.837591	1	Hapus Kategori Filing	/efiling/kategori/{id}/delete	delete
+efiling-kategori-act	0	2014-12-24 03:04:42.837591	1	Kategori Filing Act	/efiling/kategori/{act}/act	read
+efiling-kategori-csv	0	2014-12-24 03:04:42.837591	1	Kategori Filing CSV	/efiling/kategori/{csv}/csv	read
+efiling-lokasi	0	2014-12-24 03:04:42.837591	1	Lokasi Filing	/efiling/lokasi	read
+efiling-lokasi-add	0	2014-12-24 03:04:42.837591	1	Tambah Lokasi Filing	/efiling/lokasi/add	read
+efiling-lokasi-edit	0	2014-12-24 03:04:42.837591	1	Edit Lokasi Filing	/efiling/lokasi/{id}/edit	edit
+efiling-lokasi-delete	0	2014-12-24 03:04:42.837591	1	Hapus Lokasi Filing	/efiling/lokasi/{id}/delete	delete
+efiling-lokasi-act	0	2014-12-24 03:04:42.837591	1	Lokasi Filing Act	/efiling/lokasi/{act}/act	read
+efiling-lokasi-csv	0	2014-12-24 03:04:42.837591	1	Lokasi Filing CSV	/efiling/lokasi/{csv}/csv	read
+efiling-filing	0	2014-12-24 03:04:42.837591	1	 Filing	/efiling/filing	read
+efiling-filing-add	0	2014-12-24 03:04:42.837591	1	Tambah Filing	/efiling/filing/add	read
+efiling-filing-edit	0	2014-12-24 03:04:42.837591	1	Edit Filing	/efiling/filing/{id}/edit	edit
+efiling-filing-delete	0	2014-12-24 03:04:42.837591	1	Hapus Filing	/efiling/filing/{id}/delete	delete
+efiling-filing-act	0	2014-12-24 03:04:42.837591	1	 Filing Act	/efiling/filing/{act}/act	read
+efiling-filing-csv	0	2014-12-24 03:04:42.837591	1	 Filing CSV	/efiling/filing/{csv}/csv	read
+efiling-file	0	2014-12-24 03:04:42.837591	1	 Daftar File	/efiling/{id}/file	read
+efiling-file-upload	0	2014-12-24 03:04:42.837591	1	 Filing CSV	/efiling/{id}/file/upload	add
+efiling-file-delete	0	2014-12-24 03:04:42.837591	1	Tambah Filing	/efiling/file/{id}/delete	delete
