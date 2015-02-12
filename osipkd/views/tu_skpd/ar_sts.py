25aa73059f3282f26d24f1dffc40a3847c17bf3f
aagusti <aa.gustiana@gmail.com>
2015-02-12 14:50:33 +0700

    update tuskpd

--- osipkd/views/tu_skpd/ar_sts.py
+++ osipkd/views/tu_skpd/ar_sts.py
@@ -287,6 +287,9 @@ def view_delete(request):
     if row.posted:
         request.session.flash('Data sudah diposting', 'error')
         return route_list(request)
+    if row.nominal:
+        request.session.flash('Data tidak bisa dihapus, karena memiliki data items', 'error')
+        return route_list(request)
             
     form = Form(colander.Schema(), buttons=('hapus','cancel'))
     values= {}
