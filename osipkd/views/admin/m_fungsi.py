c53d5685b2c0bf130da0e5b5cd1313a72ef81510
aagusti <aa.gustiana@gmail.com>
2015-02-26 09:18:50 +0700

    update admin

--- osipkd/views/admin/m_fungsi.py
+++ osipkd/views/admin/m_fungsi.py
@@ -28,17 +28,17 @@ from osipkd.views.base_view import BaseViews
 SESS_ADD_FAILED = 'Tambah fungsi gagal'
 SESS_EDIT_FAILED = 'Edit fungsi gagal'
 
-
-                
 class AddSchema(colander.Schema):
-    kode = colander.SchemaNode(
-                    colander.String(),
-                    validator=colander.Length(max=18))
-                    
-    nama = colander.SchemaNode(
-                    colander.String())
+    kode     = colander.SchemaNode(
+                colander.String(),
+                validator=colander.Length(max=18),
+                title="Kode Fungsi")
+    nama     = colander.SchemaNode(
+                colander.String(),
+                title="Nama Fungsi")
     disabled = colander.SchemaNode(
-                    colander.Boolean())
+                colander.Boolean())
+                    
 class EditSchema(AddSchema):
     id = colander.SchemaNode(colander.String(),
             missing=colander.drop,
@@ -61,7 +61,7 @@ class view_fungsi(BaseViews):
     def gaji_fungsi_act(self):
         ses = self.request.session
         req = self.request
-        params = req.params
+        params   = req.params
         url_dict = req.matchdict
         
         if url_dict['act']=='grid':
@@ -72,6 +72,7 @@ class view_fungsi(BaseViews):
             columns.append(ColumnDT('disabled'))
             
             query = DBSession.query(Fungsi)
+            
             rowTable = DataTables(req, Fungsi, query, columns)
             return rowTable.output_result()
             
@@ -82,8 +83,6 @@ class view_fungsi(BaseViews):
                 ses['fungsi_kd']=row.kode
                 ses['fungsi_nm']=row.nama
                 return {'success':True}
-                
-            
 
     #######    
     # Add #
@@ -107,12 +106,13 @@ class view_fungsi(BaseViews):
     def save(self, values, user, row=None):
         if not row:
             row = Fungsi()
-            row.created = datetime.now()
+            row.created    = datetime.now()
             row.create_uid = user.id
         row.from_dict(values)
-        row.updated = datetime.now()
+        row.updated    = datetime.now()
         row.update_uid = user.id
-        row.disabled = 'disabled' in values and values['disabled'] and 1 or 0
+        row.disabled   = 'disabled' in values and values['disabled'] and 1 or 0
+
         DBSession.add(row)
         DBSession.flush()
         return row
@@ -121,7 +121,7 @@ class view_fungsi(BaseViews):
         if 'id' in self.request.matchdict:
             values['id'] = self.request.matchdict['id']
         row = self.save(values, self.request.user, row)
-        self.request.session.flash('fungsi sudah disimpan.')
+        self.request.session.flash('Fungsi sudah disimpan.')
             
     def route_list(self):
         return HTTPFound(location=self.request.route_url('fungsi'))
@@ -136,10 +136,21 @@ class view_fungsi(BaseViews):
     def view_fungsi_add(self):
         req = self.request
         ses = self.session
+        
         form = self.get_form(AddSchema)
         if req.POST:
             if 'simpan' in req.POST:
                 controls = req.POST.items()
+                
+                #Cek Kode Sama ato tidak
+                a = form.validate(controls)
+                b = a['kode']
+                c = "%s" % b
+                cek  = DBSession.query(Fungsi).filter(Fungsi.kode==c).first()
+                if cek :
+                    self.request.session.flash('Kode sudah ada.', 'error')
+                    return HTTPFound(location=self.request.route_url('fungsi-add'))
+                
                 try:
                     c = form.validate(controls)
                 except ValidationFailure, e:
@@ -150,7 +161,6 @@ class view_fungsi(BaseViews):
         elif SESS_ADD_FAILED in req.session:
             return self.session_failed(SESS_ADD_FAILED)
         return dict(form=form.render())
-
         
     ########
     # Edit #
@@ -159,7 +169,7 @@ class view_fungsi(BaseViews):
         return DBSession.query(Fungsi).filter_by(id=self.request.matchdict['id'])
         
     def id_not_found(self):    
-        msg = 'fungsi ID %s Tidak Ditemukan.' % self.request.matchdict['id']
+        msg = 'Fungsi ID %s tidak ditemukan.' % self.request.matchdict['id']
         request.session.flash(msg, 'error')
         return route_list()
 
@@ -167,14 +177,31 @@ class view_fungsi(BaseViews):
                  permission='edit')
     def view_fungsi_edit(self):
         request = self.request
-        row = self.query_id().first()
+        row     = self.query_id().first()
+        uid     = row.id
+        kode    = row.kode
+
         if not row:
             return id_not_found(request)
+            
         form = self.get_form(EditSchema)
         if request.POST:
             if 'simpan' in request.POST:
                 controls = request.POST.items()
                 print controls
+                
+                #Cek Kode Sama ato tidak
+                a = form.validate(controls)
+                b = a['kode']
+                c = "%s" % b
+                cek = DBSession.query(Fungsi).filter(Fungsi.kode==c).first()
+                if cek:
+                    kode1 = DBSession.query(Fungsi).filter(Fungsi.id==uid).first()
+                    d     = kode1.kode
+                    if d!=c:
+                        self.request.session.flash('Data sudah ada', 'error')
+                        return HTTPFound(location=request.route_url('fungsi-edit',id=row.id))
+
                 try:
                     c = form.validate(controls)
                 except ValidationFailure, e:
@@ -195,22 +222,22 @@ class view_fungsi(BaseViews):
                  permission='delete')
     def view_fungsi_delete(self):
         request = self.request
-        q = self.query_id()
-        row = q.first()
+        q       = self.query_id()
+        row     = q.first()
         
         if not row:
             return self.id_not_found(request)
+            
         form = Form(colander.Schema(), buttons=('hapus','batal'))
         if request.POST:
             if 'hapus' in request.POST:
-                msg = 'fungsi ID %d %s sudah dihapus.' % (row.id, row.nama)
+                msg = 'Fungsi ID %d %s sudah dihapus.' % (row.id, row.nama)
                 try:
                   q.delete()
                   DBSession.flush()
                 except:
-                  msg = 'fungsi ID %d %s tidak dapat dihapus.' % (row.id, row.nama)
+                  msg = 'Fungsi ID %d %s tidak dapat dihapus.' % (row.id, row.nama)
                 request.session.flash(msg)
             return self.route_list()
-        return dict(row=row,
-                     form=form.render())
+        return dict(row=row, form=form.render())
 
