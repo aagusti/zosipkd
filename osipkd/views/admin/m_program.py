a2de5c0b174b5673a32edfd10c62b50a3f529679
aagusti <aa.gustiana@gmail.com>
2015-02-26 09:08:12 +0700

    update admin

--- osipkd/views/admin/m_program.py
+++ osipkd/views/admin/m_program.py
@@ -33,28 +33,33 @@ SESS_EDIT_FAILED = 'Edit program gagal'
 
 class AddSchema(colander.Schema):
     urusan_widget = widget.AutocompleteInputWidget(
-            size=60,
-            values = '/urusan/act/headofnama',
-            min_length=1)
-    kode = colander.SchemaNode(
-                    colander.String(),
-                    validator=colander.Length(max=18))
-    urusan_nm = colander.SchemaNode(
+                        size=60,
+                        values = '/urusan/act/headofnama',
+                        min_length=1)
+    urusan_nm   = colander.SchemaNode(
                     colander.String(),
                     widget=urusan_widget,
-                    oid = "urusan_nm")
-    urusan_id = colander.SchemaNode(
+                    oid = "urusan_nm",
+                    title="Urusan")
+    urusan_id   = colander.SchemaNode(
                     colander.Integer(),
                     widget=widget.HiddenWidget(),
                     oid = "urusan_id")
-    nama = colander.SchemaNode(
-                    colander.String())
-    disabled = colander.SchemaNode(
+    kode        = colander.SchemaNode(
+                    colander.String(),
+                    validator=colander.Length(max=32),
+                    title="Kode Program")
+    nama        = colander.SchemaNode(
+                    colander.String(),
+                    title="Nama Program")
+    disabled    = colander.SchemaNode(
                     colander.Boolean())
+                    
 class EditSchema(AddSchema):
     id = colander.SchemaNode(colander.String(),
             missing=colander.drop,
             widget=widget.HiddenWidget(readonly=True))
+            
 class view_program(BaseViews):
     ########                    
     # List #
@@ -63,6 +68,7 @@ class view_program(BaseViews):
                  permission='read')
     def view_list(self):
         return dict(a={})
+        
     ##########                    
     # Action #
     ##########    
@@ -71,15 +77,18 @@ class view_program(BaseViews):
     def view_act(self):
         ses = self.request.session
         req = self.request
-        params = req.params
+        params   = req.params
         url_dict = req.matchdict
+        
         if url_dict['act']=='grid':
             columns = []
             columns.append(ColumnDT('id'))
             columns.append(ColumnDT('kode'))
             columns.append(ColumnDT('nama'))
             columns.append(ColumnDT('disabled'))
+            
             query = DBSession.query(Program)
+            
             rowTable = DataTables(req, Program, query, columns)
             return rowTable.output_result()
             
@@ -123,6 +132,7 @@ class view_program(BaseViews):
             program = q.first()
         else:
             program = None
+            
     def get_form(self, class_form, row=None):
         schema = class_form(validator=self.form_validator)
         schema = schema.bind()
@@ -130,70 +140,60 @@ class view_program(BaseViews):
         if row:
           schema.deserialize(row)
         return Form(schema, buttons=('simpan','batal'))
+        
     def save(self, values, user, row=None):
         if not row:
             row = Program()
-            row.created = datetime.now()
+            row.created    = datetime.now()
             row.create_uid = user.id
         row.from_dict(values)
-
-        ##### Cek Kode Sama ato tidak
-        """ukode = row.kode
-        qcek = DBSession.query(func.count(Program.id).label('jumlah')).filter(Program.kode==ukode)
-        cek =qcek.first()
-        jmlcek = cek.jumlah
-        print "PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP", jmlcek
-        if jmlcek>0 :
-           print "LEWATLEWATLEWATLEWATLEWATLEWATLEWAT"
-           self.request.session.flash('Kode sudah ada.')
-           return HTTPFound(location=self.request.route_url('program-add'))
-        """
-        row.updated = datetime.now()
+        row.updated    = datetime.now()
         row.update_uid = user.id
-        row.disabled = 'disabled' in values and values['disabled'] and 1 or 0
+        row.disabled   = 'disabled' in values and values['disabled'] and 1 or 0
+        
         DBSession.add(row)
         DBSession.flush()
         return row
+        
     def save_request(self, values, row=None):
         if 'id' in self.request.matchdict:
             values['id'] = self.request.matchdict['id']
         row = self.save(values, self.request.user, row)
-        self.request.session.flash('program sudah disimpan.')
+        self.request.session.flash('Program sudah disimpan.')
+        
     def route_list(self):
         return HTTPFound(location=self.request.route_url('program'))
+        
     def session_failed(self, session_name):
         r = dict(form=self.session[session_name])
         del self.session[session_name]
         return r
+        
     @view_config(route_name='program-add', renderer='templates/program/add.pt',
                  permission='add')
     def view_add(self):
         req = self.request
         ses = self.session
+        
         form = self.get_form(AddSchema)
         if req.POST:
             if 'simpan' in req.POST:
                 controls = req.POST.items()
-                #controls_dicted = dict(controls)
-                
-                ##### Cek Kode Sama ato tidak
-                """ck = form.validate(controls_dicted)
-                ukode = {"kode": ck['kode']}
-                print "PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP", ukode
-                qcek = DBSession.query(func.count(Program.id).label('jumlah')).filter(Program.kode==ukode)
-                cek =qcek.first()
-                jmlcek = cek.jumlah
-                if jmlcek>0 :
-                   self.request.session.flash('Kode sudah ada.')
-                   return HTTPFound(location=req.route_url('program-add'))
-                """
-                
+
+                #Cek Kode Sama ato tidak
+                a = form.validate(controls)
+                b = a['kode']
+                c = "%s" % b
+                cek  = DBSession.query(Program).filter(Program.kode==c).first()
+                if cek :
+                    self.request.session.flash('Kode sudah ada.', 'error')
+                    return HTTPFound(location=self.request.route_url('program-add'))
+                                
                 try:
                     c = form.validate(controls)
                 except ValidationFailure, e:
                     req.session[SESS_ADD_FAILED] = e.render()               
                     return HTTPFound(location=req.route_url('program-add'))
-          
                 self.save_request(dict(controls))
             return self.route_list()
         elif SESS_ADD_FAILED in req.session:
@@ -205,33 +205,40 @@ class view_program(BaseViews):
     ########
     def query_id(self):
         return DBSession.query(Program).filter_by(id=self.request.matchdict['id'])
+    
     def id_not_found(self):    
-        msg = 'program ID %s Tidak Ditemukan.' % self.request.matchdict['id']
+        msg = 'Program ID %s tidak ditemukan.' % self.request.matchdict['id']
         request.session.flash(msg, 'error')
         return route_list()
+        
     @view_config(route_name='program-edit', renderer='templates/program/edit.pt',
                  permission='edit')
     def view_edit(self):
         request = self.request
-        row = self.query_id().first()
+        row     = self.query_id().first()
+        uid     = row.id
+        kode    = row.kode
+        
         if not row:
             return id_not_found(request)
+            
         form = self.get_form(EditSchema)
         if request.POST:
             if 'simpan' in request.POST:
                 controls = request.POST.items()
-                print controls
                 
-                ##### Cek Kode Sama ato tidak
-                ukode = row.kode
-                qcek = DBSession.query(func.count(Program.id).label('jumlah')).filter(Program.kode==ukode)
-                cek =qcek.first()
-                jmlcek = cek.jumlah
-                if jmlcek>0 :
-                   self.request.session.flash('Kode sudah ada.')
-                   return HTTPFound(location=request.route_url('program-edit',
-                                      id=row.id))
-                
+                #Cek Kode Sama ato tidak
+                a = form.validate(controls)
+                b = a['kode']
+                c = "%s" % b
+                cek = DBSession.query(Program).filter(Program.kode==c).first()
+                if cek:
+                    kode1 = DBSession.query(Program).filter(Program.id==uid).first()
+                    d     = kode1.kode
+                    if d!=c:
+                        self.request.session.flash('Data sudah ada', 'error')
+                        return HTTPFound(location=request.route_url('program-edit',id=row.id))
+
                 try:
                     c = form.validate(controls)
                 except ValidationFailure, e:
@@ -244,8 +251,9 @@ class view_program(BaseViews):
             return self.session_failed(SESS_EDIT_FAILED)
         values = row.to_dict()
         values['urusan_nm'] = row.urusans.nama
-        
         return dict(form=form.render(appstruct=values))
+        
+        
     ##########
     # Delete #
     ##########    
@@ -253,20 +261,23 @@ class view_program(BaseViews):
                  permission='delete')
     def view_delete(self):
         request = self.request
-        q = self.query_id()
-        row = q.first()
+        q       = self.query_id()
+        row     = q.first()
+        
         if not row:
             return self.id_not_found(request)
+            
         form = Form(colander.Schema(), buttons=('hapus','batal'))
         if request.POST:
             if 'hapus' in request.POST:
-                msg = 'program ID %d %s sudah dihapus.' % (row.id, row.nama)
+                msg = 'Program ID %d %s sudah dihapus.' % (row.id, row.nama)
                 try:
                   q.delete()
                   DBSession.flush()
                 except:
-                  msg = 'program ID %d %s tidak dapat dihapus.' % (row.id, row.nama)
+                  msg = 'Program ID %d %s tidak dapat dihapus.' % (row.id, row.nama)
                 request.session.flash(msg)
             return self.route_list()
-        return dict(row=row,
-                     form=form.render())
+        return dict(row=row, form=form.render())
+                     
+                     
